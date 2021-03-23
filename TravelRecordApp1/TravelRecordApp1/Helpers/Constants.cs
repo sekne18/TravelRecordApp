9ecf2b143ca3255b6d0e40089a0571d91039919d
sekne18 <sekne50@gmail.com>
2021-03-23 07:08:07 +0100

    Added new class

---
+++ TravelRecordApp1/TravelRecordApp1/Helpers/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TravelRecordApp1.Helpers
+{
+    public class Constants
+    {
+        public const string VENUE_SEARCH = "";
+    }
+}
